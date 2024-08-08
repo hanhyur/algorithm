8cc69dd47e073f12e1735f04f19c51d18f66c76c
Sunwoo Han <zeidgame@gmail.com>
2024-08-08 09:11:04 +0900

    [Bronze II] Title: 애너그램 만들기, Time: 68 ms, Memory: 11504 KB -BaekjoonHub

--- Baekjoon/Bronze/1919. 애너그램 만들기/애너그램 만들기.java
+++ Baekjoon/Bronze/1919. 애너그램 만들기/애너그램 만들기.java
@@ -1,25 +1,41 @@
 import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
-public class Main{
-	public static void main(String[] args){
-		Scanner sc = new Scanner(System.in);
-		String w1 = sc.next();
-		String w2 = sc.next();
-        int cnt = 0;
-
-		int[] arr1 = new int[26];
-		int[] arr2 = new int[26];
-		for(int i=0; i<w1.length(); i++){
-            arr1[w1.charAt(i)-'a']++;
+public class Main {
+    
+    static int answer = 0;
+    
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        String first = br.readLine();
+        String second = br.readLine();
+        
+        int[] firstCountArr = getAlphabetCount(first);
+        int[] secondCountArr = getAlphabetCount(second);
+        
+        for (int i = 0; i < 26; i++) {
+            countDiffBetweenArr(i, firstCountArr, secondCountArr);
         }
-        for(int i=0; i<w2.length(); i++){
-            arr2[w2.charAt(i)-'a']++;
+        
+        System.out.println(answer);
+    }
+    
+    public static int[] getAlphabetCount(String str) {
+        int[] countArr = new int[26];
+        
+        for (int i = 0; i < str.length(); i++) {
+            countArr[str.charAt(i) - 'a']++;
         }
-		for(int i=0; i<26; i++){
-            if(arr1[i] != arr2[i]){
-                cnt += Math.abs(arr1[i] - arr2[i]);
-            }
-        }	
-		System.out.println(cnt);
-	}
+        
+        return countArr;
+    }
+    
+    private static void countDiffBetweenArr(int i, int[] first, int[] second) {
+        if (first[i] != second[i]) {
+            answer += Math.abs(first[i] - second[i]);
+        }
+    }
 }
