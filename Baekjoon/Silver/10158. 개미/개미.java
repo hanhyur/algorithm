36505aae538715e5a0e1022e74ee95f1b1e7a0d0
Sunwoo Han <zeidgame@gmail.com>
2024-08-09 11:15:36 +0900

    [Silver III] Title: 개미, Time: 64 ms, Memory: 11536 KB -BaekjoonHub

--- Baekjoon/Silver/10158. 개미/개미.java
+++ Baekjoon/Silver/10158. 개미/개미.java
@@ -8,35 +8,27 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         
         String[] size = br.readLine().split(" ");
-        String[] startPoint = br.readLine().split(" ");
-        int t = Integer.parseInt(br.readLine());
-        
         int w = Integer.parseInt(size[0]);
         int h = Integer.parseInt(size[1]);
         
-        int p = Integer.parseInt(startPoint[0]);
-        int q = Integer.parseInt(startPoint[1]);
+        String[] point = br.readLine().split(" ");
+        int p = Integer.parseInt(point[0]);
+        int q = Integer.parseInt(point[1]);
         
-        int x = getCoordinatesAfterTime(w, p, t);
-        int y = getCoordinatesAfterTime(h, q, t);
+        int t = Integer.parseInt(br.readLine());
+        
+        int x = (t + p) % (2 * w);
+        int y = (t + q) % (2 * h);
+        
+        if (x > w) {
+            x = 2 * w - x;
+        }
+        
+        if (y > h) {
+            y = 2 * h - y;
+        }
         
         System.out.println(x + " " + y);
     }
     
-    private static int getCoordinatesAfterTime(int size, int point, int time) {
-        int vector = 1;
-        int moduleTime = time % (2 * size);
-        
-        while (moduleTime-- > 0) {
-            if (point == size) {
-                vector = -1;
-            } else if (point == 0) {
-                vector = 1;
-            }
-            
-            point += vector;
-        }
-        
-        return point;
-    }
 }
