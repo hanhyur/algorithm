d9069a3e05ac8331c26843c5b9849c7b6b4387f5
Sunwoo Han <zeidgame@gmail.com>
2024-08-12 11:57:43 +0900

    [Bronze I] Title: 수 정렬하기 3, Time: 1796 ms, Memory: 302144 KB -BaekjoonHub

--- Baekjoon/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.java
+++ Baekjoon/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.java
@@ -1,23 +1,35 @@
 import java.io.*;
- 
-public class Main{
-    public static void main(String[] args) throws IOException{
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int n = Integer.parseInt(br.readLine());
-        int[] arr = new int[10001];
-        for(int i = 0; i < n; i++){
-            arr[Integer.parseInt(br.readLine())]++;
+public class Main {
+    
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        
+        // 첫 번째 줄에서 N 읽기
+        int N = Integer.parseInt(br.readLine());
+        
+        // 수의 범위에 따른 카운트 배열 초기화 (0부터 10,000까지)
+        final int MAX_VALUE = 10000;
+        int[] count = new int[MAX_VALUE + 1];
+        
+        // N개의 수를 읽어서 카운트 배열에 저장
+        for (int i = 0; i < N; i++) {
+            int number = Integer.parseInt(br.readLine());
+            count[number]++;
         }
         
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        for(int i = 1; i < 10001; i++){
-            while(0 < arr[i]--){
-                    bw.write(Integer.toString(i) + "\n");
+        // 카운트 배열을 이용하여 결과 출력
+        for (int i = 0; i <= MAX_VALUE; i++) {
+            while (count[i] > 0) {
+                bw.write(i + "\n");
+                count[i]--;
             }
         }
         
+        // 자원 정리
         br.close();
         bw.close();
     }
+    
 }
