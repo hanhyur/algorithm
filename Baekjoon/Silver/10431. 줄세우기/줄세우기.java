934bccefe3344bf13544ebb8166247c299fbc30c
Sunwoo Han <zeidgame@gmail.com>
2024-08-12 11:46:35 +0900

    [Silver V] Title: 줄세우기, Time: 204 ms, Memory: 18256 KB -BaekjoonHub

--- Baekjoon/Silver/10431. 줄세우기/줄세우기.java
+++ Baekjoon/Silver/10431. 줄세우기/줄세우기.java
@@ -1,7 +1,6 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 
 public class Main {
     
@@ -20,24 +19,34 @@ public class Main {
                 h[i] = Integer.parseInt(input[i + 1]);
             }
             
-            int[] sorted = new int[20];
             int move = 0;
+            int[] sorted = new int[20];
             
             for (int i = 0; i < 20; i++) {
-                int current = h[i];
-                int pos = Arrays.binarySearch(sorted, 0, i, current);
+                boolean find = false;
                 
-                if (pos < 0) {
-                    pos = -pos - 1;
+                for (int j = 0; j < i; j++) {
+                    if (sorted[j] > h[i]) {
+                        find = true;
+                        
+                        for (int k = i - 1; k >= j; k--) {
+                            sorted[k + 1] = sorted[k];
+                            move++;
+                        }
+                        
+                        sorted[j] = h[i];
+                        break;
+                    }
                 }
                 
-                move += i - pos;
-                
-                System.arraycopy(sorted, pos, sorted, pos + 1, i - pos);
-                sorted[pos] = current;
+                if (!find) {
+                    sorted[i] = h[i];
+                }
             }
             
             System.out.println(T + " " + move);
         }
+        
     }
+    
 }
