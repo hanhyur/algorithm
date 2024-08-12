9c6af76528581498b8101711efd9ba3ea84db056
Sunwoo Han <zeidgame@gmail.com>
2024-08-12 11:49:03 +0900

    [Silver V] Title: 줄세우기, Time: 192 ms, Memory: 18680 KB -BaekjoonHub

--- Baekjoon/Silver/10431. 줄세우기/줄세우기.java
+++ Baekjoon/Silver/10431. 줄세우기/줄세우기.java
@@ -1,52 +1,36 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-    
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        
-        int P = Integer.parseInt(br.readLine());
-        
-        while (P-- > 0) {
-            String[] input = br.readLine().split(" ");
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int p = Integer.parseInt(br.readLine());
+
+        for (int t = 0; t < p; t++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int n = Integer.parseInt(st.nextToken());
+            int[] arr = new int[20];
             
-            int T = Integer.parseInt(input[0]);
-            int[] h = new int[20];
-            
-            for (int i = 0; i < 20; i++) {
-                h[i] = Integer.parseInt(input[i + 1]);
+            for (int j = 0; j < 20; j++) {
+                arr[j] = Integer.parseInt(st.nextToken());
             }
-            
-            int move = 0;
-            int[] sorted = new int[20];
-            
+
+            int answer = 0;
+
             for (int i = 0; i < 20; i++) {
-                boolean find = false;
-                
                 for (int j = 0; j < i; j++) {
-                    if (sorted[j] > h[i]) {
-                        find = true;
-                        
-                        for (int k = i - 1; k >= j; k--) {
-                            sorted[k + 1] = sorted[k];
-                            move++;
-                        }
-                        
-                        sorted[j] = h[i];
-                        break;
+                    if (arr[i] < arr[j]) {
+                        answer++;
                     }
                 }
-                
-                if (!find) {
-                    sorted[i] = h[i];
-                }
             }
-            
-            System.out.println(T + " " + move);
+
+            bw.write(n + " " + answer + "\n");
         }
-        
+
+        br.close();
+        bw.close();
     }
-    
 }
