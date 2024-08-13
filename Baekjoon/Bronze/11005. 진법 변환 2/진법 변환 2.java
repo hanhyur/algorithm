cdef3e31c43d9844e6a75f09da79a8a87c0c9e63
Sunwoo Han <zeidgame@gmail.com>
2024-08-13 13:57:16 +0900

    [Bronze I] Title: 진법 변환 2, Time: 124 ms, Memory: 14352 KB -BaekjoonHub

---
+++ Baekjoon/Bronze/11005. 진법 변환 2/진법 변환 2.java
@@ -0,0 +1,110 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static FastReader scan = new FastReader();
+    static StringBuilder sb = new StringBuilder();
+    
+    // 변수 정의
+    static int n, b;
+    static int[] arr;
+    
+    // 변수 값 입력 받음
+    static void input() {
+        n = scan.nextInt();
+        b = scan.nextInt();
+    }
+    
+    /**
+     * 10진수 N을 B 진법으로 바꿔서 출력
+     */
+    static void func() {
+        if (n == 0) {
+            System.out.println("0");
+        }
+        
+        char[] digits = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ".toCharArray();
+        
+        while (n > 0) {
+            sb.append(digits[n % b]);
+            
+            n /= b;
+        }
+        
+        System.out.println(sb.reverse().toString());
+    }
+    
+    public static void main(String[] args) throws IOException {
+        input();
+        
+        func();
+    }
+    
+    static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+        
+        public FastReader() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+        
+        public FastReader(String s) throws FileNotFoundException {
+            br = new BufferedReader(new FileReader(new File(s)));
+        }
+        
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            
+            return st.nextToken();
+        }
+        
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+        
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+        
+        double nextDouble() {
+            return Double.parseDouble(next());
+        }
+        
+        String nextLine() {
+            String str = "";
+            
+            try {
+                str = br.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            
+            return str;
+        }
+    }
+    
+    static void insertionSort(int[] arr) {
+        for (int i = 1; i < arr.length; i++) {
+            for (int j = 0; j < i; j++) {
+                if (arr[j] > arr[i]) {
+                    int current = arr[i];
+                    
+                    for (int k = i; k > j; k--) {
+                        arr[k] = arr[k - 1];
+                    }
+                    
+                    arr[j] = current;
+                    
+                    break;
+                }
+            }
+        }
+    }
+    
+}
