6721dc244336af0d68c7a45b2fd546ff2b1440d6
Sunwoo Han <zeidgame@gmail.com>
2024-08-28 11:21:13 +0900

    [Silver V] Title: 단어 정렬, Time: 256 ms, Memory: 29720 KB -BaekjoonHub

--- Baekjoon/Silver/1181. 단어 정렬/단어 정렬.java
+++ Baekjoon/Silver/1181. 단어 정렬/단어 정렬.java
@@ -1,42 +1,100 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.*;
 
 public class Main {
-    public static void main(String[] args) throws Exception {
+    static FastReader scan = new FastReader();
+    static StringBuilder sb = new StringBuilder();
+    
+    // 변수 정의
+    static int n;
+    static String[] arr;
+    
+    // 변수 값 입력 받음
+    static void input() {
+        n = scan.nextInt();
+        arr = new String[n];
         
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-        int n = Integer.parseInt(br.readLine());
-        String[] arr = new String[n];
-        
-        for(int i = 0; i < n; i++) {
-            arr[i] = br.readLine();
+        for (int i = 0; i < n; i++) {
+            arr[i] = scan.nextLine();
         }
-
-        Arrays.sort(arr, new Comparator<String>() {
-            @Override
-            public int compare(String s1, String s2) {
-                
-                if(s1.length() == s2.length()) {
-                    return s1.compareTo(s2);
-                } else {
-                    return s1.length() - s2.length();
+    }
+    
+    static class WordSort implements Comparator<String> {
+        @Override
+        public int compare(String o1, String o2) {
+            if (o1.length() != o2.length()) {
+                return o1.length() - o2.length();
+            }
+            
+            return o1.compareTo(o2);
+        }
+    }
+    
+    static void func() {
+        Arrays.sort(arr, new WordSort());
+        
+        for (int i = 0; i < n; i++) {
+            if (i == 0 || arr[i].compareTo(arr[i - 1]) != 0) {
+                sb.append(arr[i]).append("\n");
+            }
+        }
+        
+        System.out.println(sb.toString());
+    }
+    
+    public static void main(String[] args) throws IOException {
+        input();
+        
+        func();
+    }
+    
+    static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+        
+        public FastReader() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+        
+        public FastReader(String s) throws FileNotFoundException {
+            br = new BufferedReader(new FileReader(new File(s)));
+        }
+        
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
                 }
             }
-        });
-
-        StringBuilder sb = new StringBuilder();
-
-        sb.append(arr[0]).append('\n');
-
-        for(int i = 1; i < n; i++) {
-            if(!arr[i].equals(arr[i - 1])) {
-                sb.append(arr[i]).append('\n');
-            }
+            
+            return st.nextToken();
         }
-
-        System.out.println(sb);
-
+        
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+        
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+        
+        double nextDouble() {
+            return Double.parseDouble(next());
+        }
+        
+        String nextLine() {
+            String str = "";
+            
+            try {
+                str = br.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            
+            return str;
+        }
     }
+    
 }
