35b6dd1b483ce1355873371d356c39112b79c9f5
Sunwoo Han <sunwoohan.me@gmail.com>
2025-02-28 13:31:30 +0900

    [level 2] Title: 의상, Time: 5.33 ms, Memory: 78.7 MB -BaekjoonHub

--- Programmers/2/42578. 의상/의상.java
+++ Programmers/2/42578. 의상/의상.java
@@ -1,7 +1,9 @@
 import java.util.*;
+import java.util.stream.*;
 
 class Solution {
     public int solution(String[][] clothes) {
+        /* 
         int answer = 1;
         
         Map<String, Integer> map = new HashMap<>();
@@ -17,5 +19,14 @@ class Solution {
         }
         
         return answer - 1;
-    }
+        */
+        
+        return Arrays.stream(clothes)
+            .collect(Collectors.groupingBy(cloth -> cloth[1], Collectors.counting()))
+            .values()
+            .stream()
+            .map(count -> count + 1)
+            .reduce(1L, (a, b) -> a * b)
+            .intValue() - 1;
+        }
 }
