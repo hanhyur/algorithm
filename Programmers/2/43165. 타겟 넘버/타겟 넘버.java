993790cf8e1ee82977caa6f827549ae68982b5bd
Sunwoo Han <sunwoohan.me@gmail.com>
2025-03-04 19:30:46 +0900

    [level 2] Title: 타겟 넘버, Time: 0.38 ms, Memory: 86.2 MB -BaekjoonHub

---
+++ Programmers/2/43165. 타겟 넘버/타겟 넘버.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int solution(int[] numbers, int target) {
+        return dfs(numbers, target, 0, 0);
+    }
+    
+    private int dfs(int[] numbers, int target, int depth, int sum) {
+        if (depth == numbers.length) {
+            return sum == target ? 1 : 0;
+        }
+        
+        return dfs(numbers, target, depth + 1, sum + numbers[depth]) +
+            dfs(numbers, target, depth + 1, sum - numbers[depth]);
+    }
+}
