7188512b8ddabf9d29914d9acbf25371ac195160
Sunwoo Han <sunwoohan.me@gmail.com>
2025-03-30 20:27:40 +0900

    Time: 1 ms (100%), Space: 46.2 MB (59.22%) - LeetHub

--- Leetcode/0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
+++ Leetcode/0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
@@ -2,21 +2,41 @@ class Solution {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
         int m = nums1.length;
         int n = nums2.length;
-        
 
-        int sum1 = 0;
-        int sum2 = 0;
-
-        for (int i = 0; i < m; i++) {
-            sum1 += nums1[i];
+        if (m > n) {
+            return findMedianSortedArrays(nums2, nums1);
         }
 
-        for (int i = 0; i < n; i++) {
-            sum2 += nums2[i];
+        int sum = m + n;
+        int left = (sum + 1) / 2;
+        int low = 0;
+        int high = m;
+
+        while (low <= high) {
+            int mid1 = (low + high) / 2;
+            int mid2 = left - mid1;
+
+            int left1 = (mid1 - 1 < 0) ? Integer.MIN_VALUE : nums1[mid1 - 1];
+            int right1 = (mid1 >= m) ? Integer.MAX_VALUE : nums1[mid1];
+
+            int left2 = (mid2 - 1 < 0) ? Integer.MIN_VALUE : nums2[mid2 - 1];
+            int right2 = (mid2 >= n) ? Integer.MAX_VALUE : nums2[mid2];
+
+            if (left1 <= right2 && left2 <= right1) {
+                if (sum % 2 == 0) {
+                    return (Math.max(left1, left2) + Math.min(right1, right2)) / 2.0;
+                } else {
+                    return Math.max(left1, left2);
+                }
+            }
+            
+            if (left1 > right2) {
+                high = mid1 - 1;
+            } else {
+                low = mid1 + 1;
+            }
         }
 
-        int total = sum1 + sum2;
-
-        return total / (double) (m + n);
+        return 0;
     }
 }
