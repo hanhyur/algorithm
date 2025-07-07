4a50e9ca6d1c29821f030cbad790eb19511dbf2e
Sunwoo Han <sunwoohan.me@gmail.com>
2025-07-07 14:15:54 +0900

    [Bronze V] Title: X보다 작은 수, Time: 0 ms, Memory: 2060 KB -BaekjoonHub

--- Baekjoon/Bronze/10871. X보다 작은 수/X보다 작은 수.cc
+++ Baekjoon/Bronze/10871. X보다 작은 수/X보다 작은 수.cc
@@ -1,23 +1,11 @@
-#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+int n, x, a[10005];
 int main(void){
-    std::ios::sync_with_stdio(0);
-    std::cin.tie(0);
-    
-    int n, x;
-    
-    std::cin >> n >> x;
-    
-    int* a = new int[n];
-    
-    for (int i = 0; i < n; i++) {
-        std::cin >> a[i];
-    }
-    
-    for (int i = 0; i < n; i++) {
-        if (a[i] < x) {
-            std::cout << a[i] << ' ';
-        }
-    }
-    
-    delete[] a;
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cin >> n >> x;
+    for (int i = 0; i < n; i++) cin >> a[i];
+    for (int i = 0; i < n; i++)
+        if (a[i] < x) cout << a[i] << ' ';
 }
