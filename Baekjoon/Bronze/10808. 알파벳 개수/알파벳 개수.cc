38e47197bd07bc08642484d648ad2fad3bda8142
Sunwoo Han <sunwoohan.me@gmail.com>
2025-07-07 22:44:51 +0900

    [Bronze IV] Title: 알파벳 개수, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- Baekjoon/Bronze/10808. 알파벳 개수/알파벳 개수.cc
+++ Baekjoon/Bronze/10808. 알파벳 개수/알파벳 개수.cc
@@ -1,15 +1,14 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
+int freq[26];
 int main(void) {
     ios::sync_with_stdio(0);
     cin.tie(0);
     string s;
     cin >> s;
-    for (char ch = 'a'; ch <= 'z'; ch++) {
-        int cnt = 0;
-        for (auto c : s)
-            if (ch == c) cnt++;
-        cout << cnt << ' ';
-    }
+    for (auto ch : s)
+        freq[ch - 'a']++;
+    for (int i = 0; i < 26; i++)
+        cout << freq[i] << ' ';
 }
