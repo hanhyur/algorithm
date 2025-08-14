7a67e1e5b42d0bb9cf9dab022b8d18e5747cbd43
Sunwoo Han <sunwoohan.me@gmail.com>
2025-08-14 09:20:47 +0900

    [level 0] Title: 짝수의 합, Time: 0.01 ms, Memory: 3.63 MB -BaekjoonHub

--- Programmers/0/120831. 짝수의 합/짝수의 합.cpp
+++ Programmers/0/120831. 짝수의 합/짝수의 합.cpp
@@ -9,9 +9,11 @@ int solution(int n) {
     while (n > 0) {
         if (n % 2 == 0) {
             answer += n;
+            
+            n -= 2;
+        } else {
+            n--;
         }
-        
-        n--;
     }
     
     return answer;
