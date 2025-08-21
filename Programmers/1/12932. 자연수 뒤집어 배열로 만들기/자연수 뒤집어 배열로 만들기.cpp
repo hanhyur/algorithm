74432c0af7aefcac454368158e1a6f5a6868701b
Sunwoo Han <sunwoohan.me@gmail.com>
2025-08-21 09:08:00 +0900

    [level 1] Title: 자연수 뒤집어 배열로 만들기, Time: 0.01 ms, Memory: 4.2 MB -BaekjoonHub

---
+++ Programmers/1/12932. 자연수 뒤집어 배열로 만들기/자연수 뒤집어 배열로 만들기.cpp
@@ -0,0 +1,16 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<int> solution(long long n) {
+    vector<int> answer;
+    
+    while (n > 0) {
+        int num = (int) (n % 10);
+        answer.push_back(num);
+        n /= 10;
+    }    
+    
+    return answer;
+}
