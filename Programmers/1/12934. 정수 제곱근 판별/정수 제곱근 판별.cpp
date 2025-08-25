c7b9ed1918806724d531bcb9bdad5e76f19049b9
Sunwoo Han <sunwoohan.me@gmail.com>
2025-08-25 09:13:56 +0900

    [level 1] Title: 정수 제곱근 판별, Time: 0.01 ms, Memory: 4.21 MB -BaekjoonHub

--- Programmers/1/12934. 정수 제곱근 판별/정수 제곱근 판별.cpp
+++ Programmers/1/12934. 정수 제곱근 판별/정수 제곱근 판별.cpp
@@ -1,15 +1,37 @@
 #include <string>
 #include <vector>
-#include <cmath>
 
 using namespace std;
+
+double m_pow(double n, double m) {
+    double nn = n;
+    
+    if (m == 0)       
+        return 1.0;
+    
+    for (int i = 0; i < (m - 1); i++)
+        n *= nn;
+    
+    return n;
+}
+
+double m_sqrt(double n) {
+    double s = n / 2;
+    double t = 0;
+
+    while (s != t) {
+        t = s;
+        s = ((n / t) + t) / 2;
+    }
+    
+    return s;
+}
 
 long long solution(long long n) {
     long long answer = 0;
     
-    if (pow((long double) sqrt(n), 2) == n) {
-        return (long long) pow(sqrt(n) + 1, 2);
-    }
+    if (m_pow((int) m_sqrt(n), 2) == n)
+        return (long long) m_pow(m_sqrt(n) + 1, 2);
     
     return -1;
 }
