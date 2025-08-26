8c0373d974a3cc471217e598ec2ddf1a09dfb35c
Sunwoo Han <sunwoohan.me@gmail.com>
2025-08-27 08:49:21 +0900

    [level 1] Title: 하샤드 수, Time: 0.01 ms, Memory: 4.21 MB -BaekjoonHub

---
+++ Programmers/1/12947. 하샤드 수/하샤드 수.cpp
@@ -0,0 +1,22 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+bool solution(int x) {
+    int temp = x;
+    int sum = 0;
+        
+    while ((temp/10) != 0) {
+        sum += temp%10;
+        temp = temp/10;
+    }
+        
+    sum += temp;
+        
+    if ((x % sum) != 0) {
+        return false;
+    }
+    
+    return true;
+}
