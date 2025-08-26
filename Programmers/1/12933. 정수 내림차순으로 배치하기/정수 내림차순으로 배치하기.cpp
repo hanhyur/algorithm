cb507a1e1d7ef0def5661d1b15522702c8d4bfa3
Sunwoo Han <sunwoohan.me@gmail.com>
2025-08-26 09:29:38 +0900

    [level 1] Title: 정수 내림차순으로 배치하기, Time: 0.01 ms, Memory: 4.22 MB -BaekjoonHub

---
+++ Programmers/1/12933. 정수 내림차순으로 배치하기/정수 내림차순으로 배치하기.cpp
@@ -0,0 +1,25 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+long long solution(long long n) {
+    long long answer = 0;
+    
+    string str = to_string(n);
+    vector<int> digits;
+    
+    for (char c : str) {
+        digits.push_back(c - '0');
+    }
+    
+    sort(digits.begin(), digits.end(), greater<int>());
+    
+    for (int num : digits) {
+        answer += num;
+        answer *= 10;
+    }
+    
+    return answer / 10;
+}
