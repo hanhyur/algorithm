8aa95cf88d1c89b067299715b7ec6059a7dda7cf
Sunwoo Han <sunwoohan.me@gmail.com>
2025-09-19 09:55:23 +0900

    [level 1] Title: 3진법 뒤집기, Time: 0.02 ms, Memory: 4.21 MB -BaekjoonHub

---
+++ Programmers/1/68935. 3진법 뒤집기/3진법 뒤집기.cpp
@@ -0,0 +1,32 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+#include <iostream>
+
+using namespace std;
+
+int solution(int n) {
+    int answer = 0;
+    
+    vector<int> v;
+    
+    while (n > 0) {
+        v.push_back(n % 3);
+        n = n / 3;
+    }
+    
+    reverse(v.begin(), v.end());
+    
+    answer += v[0] * 1;
+    
+    for (int i = 1; i < v.size(); i++) {
+        if (v[i] == 0) {
+            continue;
+        }
+        
+        answer += v[i] * pow(3, i);
+    }
+    
+    return answer;
+}
