16c4fe51050ce54d018e03c3d53b216a85d0e0d7
Sunwoo Han <sunwoohan.me@gmail.com>
2025-09-30 09:19:00 +0900

    [level 1] Title: 두 개 뽑아서 더하기, Time: 0.02 ms, Memory: 4.21 MB -BaekjoonHub

--- Programmers/1/68644. 두 개 뽑아서 더하기/README.md
+++ Programmers/1/68644. 두 개 뽑아서 더하기/README.md
@@ -1,10 +1,10 @@
 # [level 1] 두 개 뽑아서 더하기 - 68644 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/68644?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/68644?language=cpp) 
 
 ### 성능 요약
 
-메모리: 69.8 MB, 시간: 1.50 ms
+메모리: 4.21 MB, 시간: 0.02 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 09월 30일 09:13:27
+2025년 09월 30일 09:18:58
 
 ### 문제 설명
 
---
+++ Programmers/1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.cpp
@@ -0,0 +1,23 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <set>
+
+using namespace std;
+
+vector<int> solution(vector<int> numbers) {
+    vector<int> answer;
+    set<int> s;
+    
+    for (int i = 0; i < numbers.size(); i++) {
+        for (int j = i + 1; j < numbers.size(); j++) {
+            s.insert(numbers[i] + numbers[j]);
+        }
+    }
+    
+    for (int num : s) {
+        answer.push_back(num);
+    }
+    
+    return answer;
+}
