d478bb4b05007643100b99f7b69e1fc2401e490b
Sunwoo Han <sunwoohan.me@gmail.com>
2025-10-01 09:24:09 +0900

    [level 1] Title: 푸드 파이트 대회, Time: 0.01 ms, Memory: 4.14 MB -BaekjoonHub

--- Programmers/1/134240. 푸드 파이트 대회/README.md
+++ Programmers/1/134240. 푸드 파이트 대회/README.md
@@ -1,10 +1,10 @@
 # [level 1] 푸드 파이트 대회 - 134240 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/134240?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/134240?language=cpp) 
 
 ### 성능 요약
 
-메모리: 93.1 MB, 시간: 7.90 ms
+메모리: 4.14 MB, 시간: 0.01 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 10월 01일 09:18:42
+2025년 10월 01일 09:24:06
 
 ### 문제 설명
 
---
+++ Programmers/1/134240. 푸드 파이트 대회/푸드 파이트 대회.cpp
@@ -0,0 +1,19 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+string solution(vector<int> food) {
+    string left = "";
+    
+    for (int i = 1; i < food.size(); i++) {
+        int count = food[i] / 2;
+        left.append(count, char('0' + i));
+    }
+
+    string right = left;
+    reverse(right.begin(), right.end());    
+    
+    return left + "0" + right;
+}
