b07333be8323f017570658f625892485563be17f
Sunwoo Han <sunwoohan.me@gmail.com>
2025-10-02 20:21:46 +0900

    [Bronze III] Title: ZOAC 4, Time: 100 ms, Memory: 14224 KB -BaekjoonHub

--- Baekjoon/Bronze/23971. ZOAC 4/ZOAC 4.java
+++ Baekjoon/Bronze/23971. ZOAC 4/ZOAC 4.java
@@ -1,43 +1,24 @@
 import java.io.*;
-import java.util.*;
+import java.util.StringTokenizer;
 
 public class Main {
     static FastReader scan = new FastReader();
     static StringBuilder sb = new StringBuilder();
     
-    // 변수 정의
     static int h, w, n, m;
     
-    // 변수 값 입력 받음
     static void input() {
-        h = Integer.parseInt(scan.next());
-        w = Integer.parseInt(scan.next());
-        n = Integer.parseInt(scan.next());
-        m = Integer.parseInt(scan.next());
+        h = scan.nextInt();
+        w = scan.nextInt();
+        n = scan.nextInt();
+        m = scan.nextInt();
     }
     
     static void func() {
-        int row = h;
-        int rowGap = n + 1;
-        int rowCnt = 0;
+        int rowCount = (h + (n + 1) - 1) / (n + 1);
+        int colCount = (w + (m + 1) - 1) / (m + 1);
         
-        int col = w;
-        int colGap = m + 1;
-        int colCnt = 0;
-        
-        while(row >= 1) {
-            rowCnt++;
-            row = row - rowGap;
-        }
-        
-        while(col >= 1) {
-            colCnt++;
-            col = col - colGap;
-        }
-        
-        int answer = rowCnt * colCnt;
-        
-        System.out.println(answer);
+        System.out.println(rowCount * colCount);
     }
     
     public static void main(String[] args) throws IOException {
@@ -94,4 +75,5 @@ public class Main {
             return str;
         }
     }
+
 }
